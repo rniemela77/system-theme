3464990f0fce1e6d61d4553a1f5aec5967672c99
rniemela77 <rvniemela@hotmail.com>
2025-05-28 22:11:05 -0700

    remove boilerplate count logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Window from './components/Window'
 import backgroundImage from './assets/background.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div style={{
       backgroundImage: `url(${backgroundImage})`,
